db3198a43f42339aa7ce341b853387b9a9a9ff96
panvarg11 <mpanvargas@gmail.com>
2025-01-07 13:04:08 -0500

    res send file add

--- myApp.js
+++ myApp.js
@@ -1,6 +1,19 @@
 let express = require('express');
 let app = express();
 
+console.log(__dirname)
+
+const absolutePath =__dirname+'/views/index.html'
+
+app.get("/",(req,res) => {
+
+    res.sendFile(absolutePath)
+    
+})
+
+app.listen(3000,()=>{
+    
+})
 
 
 
